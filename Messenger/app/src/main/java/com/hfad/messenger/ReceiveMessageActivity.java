3506c81286f70f9ff2de3760c87a177f235427b2
joshua murray <joshua@arcflashed.com>
2017-07-07 22:53:13 -0700

    more chap 3 code

--- Messenger/app/src/main/java/com/hfad/messenger/ReceiveMessageActivity.java
+++ Messenger/app/src/main/java/com/hfad/messenger/ReceiveMessageActivity.java
@@ -1,13 +1,20 @@
 package com.hfad.messenger;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
-public class ReceiveMessageActivity extends AppCompatActivity {
+public class ReceiveMessageActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+    }
+    //call onSendMeaage() when button is clicked
+    public void onSendMessage(View view){
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        startActivity(intent);
     }
 }
