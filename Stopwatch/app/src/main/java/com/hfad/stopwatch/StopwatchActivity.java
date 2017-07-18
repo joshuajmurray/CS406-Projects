c514d06b8c9406aec25fe16f9053daff2dde1cea
joshua murray <joshua@arcflashed.com>
2017-07-17 22:04:10 -0700

    added activities for stopwatch, needs to be tested still

--- Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -1,13 +1,59 @@
 package com.hfad.stopwatch;
 
+import android.app.Activity;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class StopwatchActivity extends AppCompatActivity {
+
+public class StopwatchActivity extends Activity {
+
+    private int seconds = 0;
+    private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        runTimer();
+    }
+
+    //start the stopwatch
+    public void onClickStart(View view) {
+        running = true;
+    }
+
+    //stop the stopwatch
+    public void onClickStop(View view) {
+        running = false;
+    }
+
+    //reset the stopwatch
+    public void onClickReset(View view) {
+        running = false;
+        seconds = 0;
+    }
+
+    //sets the number of seconds on the timer
+    private void runTimer() {
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds % 3600)/60;
+                int secs = seconds % 60;
+                String time = String.format("%d:%02d:%02d",
+                        hours, minutes, secs);
+                timeView.setText(time);
+                if(running) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
