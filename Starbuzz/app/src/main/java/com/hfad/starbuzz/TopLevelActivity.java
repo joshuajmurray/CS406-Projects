d31beccaff940f4772f30ef4df5aa04d0bfeff76
joshua murray <joshua@arcflashed.com>
2017-07-22 16:57:39 -0700

    got drink activity working with starbuzz code

--- Starbuzz/app/src/main/java/com/hfad/starbuzz/TopLevelActivity.java
+++ Starbuzz/app/src/main/java/com/hfad/starbuzz/TopLevelActivity.java
@@ -1,13 +1,35 @@
 package com.hfad.starbuzz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.view.View;
 
-public class TopLevelActivity extends AppCompatActivity {
+public class TopLevelActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+        //create and onitemclicklistener
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> listView,
+                                   View v,
+                                   int position,
+                                   long id){
+                if(position == 0) {
+                    Intent intent = new Intent(TopLevelActivity.this,
+                                               DrinkCategoryActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        //add listener to the list view
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
