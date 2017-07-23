26ac182e64470ed35de1d3ecf988a909212abec9
joshua murray <joshua@arcflashed.com>
2017-07-22 17:59:40 -0700

    wrapped up chapter 6, finished adding all sub-activities and imageViews

--- Starbuzz/app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
+++ Starbuzz/app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
@@ -2,9 +2,10 @@ package com.hfad.starbuzz;
 
 import android.app.ListActivity;
 import android.os.Bundle;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.view.View;
+import android.content.Intent;
 
 public class DrinkCategoryActivity extends ListActivity {
 
@@ -17,5 +18,15 @@ public class DrinkCategoryActivity extends ListActivity {
                 android.R.layout.simple_list_item_1,
                 Drink.drinks);
         listDrinks.setAdapter(listAdapter);
+    }
+
+    @Override
+    public void onListItemClick(ListView listView,
+                                View itemView,
+                                int position,
+                                long id) {
+        Intent intent = new Intent(DrinkCategoryActivity.this, DrinkActivity.class);
+        intent.putExtra(DrinkActivity.EXTRA_DRINKNO, (int) id);
+        startActivity(intent);
     }
 }
