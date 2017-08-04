6f1d51c9e6069f8b8907ab282c1f131850d45fba
joshua murray <joshua@arcflashed.com>
2017-08-03 22:13:32 -0700

    app crashes on load, not sure what's going on yet

--- Bitsandpizzas2/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ Bitsandpizzas2/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -17,8 +17,9 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //inflate teh menu: this adds items to the action bar if it is present
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+  //      return super.onCreateOptionsMenu(menu);
+        return true;
     }
 
     @Override
