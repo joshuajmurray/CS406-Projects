a81d7315f9048ae864c958dc07e807c4285356b0
joshua murray <joshua@arcflashed.com>
2017-08-06 17:41:35 -0700

    initial commit of project code

---
+++ BitsandPizzas3/app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
@@ -0,0 +1,19 @@
+package com.hfad.bitsandpizzas;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class OrderActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
